30e5cbb48eb5c0fe9c8d7060174cb8839cdddabc
Sean Phan <42845715+seanphan05@users.noreply.github.com>
2020-07-24 08:15:01 -0700

    Update config.php

--- config/config.php
+++ config/config.php
@@ -3,10 +3,10 @@
 * Configuration for database connection
 *
 */
-$host 		= "sql211.ezyro.com";
-$username 	= "ezyro_26288695";
-$password   = "ocna7a894h";
-$dbname     = "ezyro_26288695_lottery_db";
+$host 		= "";
+$username 	= "";
+$password   = "";
+$dbname     = "";
 $dsn        = "mysql:host=$host; dbname=$dbname";
 $options    = array(
 		PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
